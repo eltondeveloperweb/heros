f3f385ee37d64ad9fa05e744b32f91d11eb30f31
Elton Adriano <eltonfullstack@gmail.com>
2021-02-11 08:41:19 -0300

    first commit

---
+++ src/main/java/br/com/microservice/hero/MicroserviceHeroApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.hero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHeroApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/microservice/hero/resource/HeroResource.java
@@ -0,0 +1,16 @@
+package br.com.microservice.hero.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/heros")
+public class HeroResource {
+	
+	@GetMapping(value = "/hello")
+	public String hello() {
+		return "Hello Jenkins - Thanks my God";
+	}
+
+}
